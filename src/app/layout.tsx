8088fa7eaf3d579066ddb9ec28ce75ffe8d197b6
Nwachuya <106511230+Nwachuya@users.noreply.github.com>
2026-01-08 22:40:13 +0100

    added
    Open src/app/layout.tsx and add the suppressHydrationWarning prop to the <html> tag:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    // Add suppressHydrationWarning here 👇
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   )
